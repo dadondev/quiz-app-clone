e394aa1e675793e3260d5acc38c31c9af4267ec2
dadondev <dadondev03@gmail.com>
2024-09-14 22:40:58 +0500

    updated
    Signed-off-by: dadondev <dadondev03@gmail.com>

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,3 @@
 /** @format */
-export const BASE_URL = process.env.BASE_URL ||"https://elibrary-backend-production.up.railway.app/api/";
+export const BASE_URL = process.env.BASE_URL ||"https://elibrary-backend-production.up.railway.app/api";
 export const secret = process.env.JWT_SECRET ||"ELIBRARY-JWT-SECRET"
